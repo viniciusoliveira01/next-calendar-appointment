e9209cb0a0f8f61cfc03e4dcdb2bce4d108d999c
Vinícius Alexandre <vinicius@gettimesaved.com>
2023-02-23 21:17:19 -0300

    finished availability flow

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -43,12 +43,20 @@ export const intervals = [
   },
 ]
 
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('en-CA', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
 }
